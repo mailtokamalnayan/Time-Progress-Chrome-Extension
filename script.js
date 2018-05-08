23e202efd68fcea903abd27dc9f26a5c990fea38
mailtokamalnayan <mailtokamalnayan@gmail.com>
2018-05-09 00:31:55 +0800

    Add year slider slider

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("Hello world!")
-
 function checkTime(i) {
   if (i < 10) {
     i = "0" + i;
@@ -9,16 +7,29 @@ function checkTime(i) {
 
 function startTime() {
   var today = new Date();
-  var h = today.getHours();
-  var m = today.getMinutes();
-  m = checkTime(m);
-  mLeft = Math.round((today.getMinutes() / 60) * 100);
-  dLeft = Math.round((today.getHours() / 24) * 100);
-  wLeft = Math.round((today.getDay() / 7) * 100);
-  document.getElementById('time').innerHTML = h + ":" + m;
+  var dayHour = today.getHours();
+  var min = today.getMinutes();
+  var weekDay = today.getDay();
+  min = checkTime(min);
+
+  // Calculate the current date of the year
+  var timestmp = new Date().setFullYear(new Date().getFullYear(), 0, 1);
+  var yearFirstDay = Math.floor(timestmp / 86400000);
+  var today = Math.ceil((new Date().getTime()) / 86400000);
+  var dayOfYear = today - yearFirstDay;
+
+  mLeft = Math.round((min / 60) * 100);
+  dLeft = Math.round((dayHour / 24) * 100);
+  wLeft = Math.round((weekDay / 7) * 100);
+  yLeft = Math.round((dayOfYear / 365) * 100);
+  document.getElementById('time').innerHTML = dayHour + ":" + min;
   document.getElementById('mLeft').innerHTML = mLeft + "%";
   document.getElementById('dLeft').innerHTML = dLeft + "%";
   document.getElementById('wLeft').innerHTML = wLeft + "%";
+  document.getElementById('yLeft').innerHTML = yLeft + "%";
+  yLeftNum = parseInt("yLeft", 10) + '%';
+  console.log(typeof yLeftNum);
+  document.getElementById('yearProgressBar').style.width = yLeft + '%';
   t = setTimeout(function() {
     startTime()
   }, 60000);
